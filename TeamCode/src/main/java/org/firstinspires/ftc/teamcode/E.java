fa8127b8f0aaecd068257f783f7d443ac3a28636
Stephie Seaton <stephanie.seaton@sisepuedefoundation.org>
2024-09-25 20:16:01 -0700

    New Outreach Bot Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/E.java
@@ -0,0 +1,218 @@
+package org.firstinspires.ftc.teamcode;
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/*
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Robot: Auto Drive By Encoder", group="Robot")
+public class E extends LinearOpMode {
+// E is basically strife driving + eventual other things probably <<33
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor fL = null;
+    private DcMotor fR = null;
+    private DcMotor bL = null;
+    private DcMotor bR = null;
+    private DcMotor frontMiddle = null;
+    private DcMotor backMiddle = null;
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        fL  = hardwareMap.get(DcMotor.class, "front_left");
+        fR = hardwareMap.get(DcMotor.class, "front_right");
+        bL  = hardwareMap.get(DcMotor.class, "back_left");
+        bR = hardwareMap.get(DcMotor.class, "back_right");
+        frontMiddle  = hardwareMap.get(DcMotor.class, "front_middle");
+        backMiddle = hardwareMap.get(DcMotor.class, "back_middle");
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        fL.setDirection(DcMotor.Direction.REVERSE);
+        fR.setDirection(DcMotor.Direction.FORWARD);
+        bL.setDirection(DcMotor.Direction.REVERSE);
+        bR.setDirection(DcMotor.Direction.FORWARD);
+        frontMiddle.setDirection(DcMotor.Direction.REVERSE);
+        backMiddle.setDirection(DcMotor.Direction.FORWARD);
+
+        fL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontMiddle.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backMiddle.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontMiddle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backMiddle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Wait for the game to start (driver presses PLAY)
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Starting at",  "%7d :%7d",
+                fL.getCurrentPosition(),
+                fR.getCurrentPosition(),
+                bL.getCurrentPosition(),
+                bR.getCurrentPosition(),
+                frontMiddle.getCurrentPosition(),
+                backMiddle.getCurrentPosition());
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);  // pause to display final telemetry message.
+    }
+
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the OpMode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches, double middleInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+        int newMiddleTarget;
+
+        // Ensure that the OpMode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = fL.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = fR.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newMiddleTarget = frontMiddle.getCurrentPosition() + (int)(middleInches * COUNTS_PER_INCH);
+            fL.setTargetPosition(newLeftTarget);
+            fR.setTargetPosition(newRightTarget);
+            bL.setTargetPosition(newLeftTarget);
+            bR.setTargetPosition(newRightTarget);
+            frontMiddle.setTargetPosition(newMiddleTarget);
+            backMiddle.setTargetPosition(newMiddleTarget);
+
+            // Turn On RUN_TO_POSITION
+            fL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            fR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontMiddle.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backMiddle.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            fL.setPower(Math.abs(speed));
+            fR.setPower(Math.abs(speed));
+            bL.setPower(Math.abs(speed));
+            bR.setPower(Math.abs(speed));
+            frontMiddle.setPower(Math.abs(speed));
+            backMiddle.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (fL.isBusy() && fR.isBusy() && bL.isBusy() && bR.isBusy() && frontMiddle.isBusy() && backMiddle.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to",  " %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Currently at",  " at %7d :%7d",
+                        fL.getCurrentPosition(), fR.getCurrentPosition(), bL.getCurrentPosition(), bR.getCurrentPosition(), frontMiddle.getCurrentPosition(), backMiddle.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            fL.setPower(0);
+            fR.setPower(0);
+            bL.setPower(0);
+            bR.setPower(0);
+            frontMiddle.setPower(0);
+            backMiddle.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            fL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            fR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontMiddle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backMiddle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(250);   // optional pause after each move.
+        }
+    }
+}
