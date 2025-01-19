76c58446b0c9c13d3ef431f12c6d023fde04d8ec
Stephie Seaton <stephanie.seaton@sisepuedefoundation.org>
2025-01-19 11:20:46 -0700

    gamepad 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2024.java
@@ -23,6 +23,7 @@ public class TeleOp2024 extends LinearOpMode {
 
     DcMotor arm1;
     DcMotor arm2;
+    DcMotor arm3;
     MechanumDrive e;
 
 
@@ -38,8 +39,10 @@ public class TeleOp2024 extends LinearOpMode {
         // initialize motors
         arm1 = hardwareMap.get(DcMotor.class, "arm1");
         arm2 = hardwareMap.get(DcMotor.class, "arm2");
+        arm3 = hardwareMap.get(DcMotor.class, "arm3");
         arm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 //
 //
@@ -70,22 +73,35 @@ public class TeleOp2024 extends LinearOpMode {
         double rightX = gamepad1.right_stick_x;
 
         e.MoveRobot(leftY,leftX,rightX);
-        boolean upButton = gamepad1.x;
-        boolean downButton = gamepad1.a;
-        if ( upButton) {
+        double upButton1 = gamepad2.left_stick_y;
+        double downButton1 = gamepad2.right_stick_y;
+
+        if (upButton1 > 0) {
             arm1.setPower(1);
-            arm2.setPower(1);
+            arm3.setPower(1);
         }
         //down button is a on the game pad and up button is x on the game pad. when you press a both arms go down. when you press x both arms go up faster than they go down.
         //the if statements check if the two buttons on the gamepad are pressed and do the actions assigned to the button.
-        else if (downButton){
+        else if (upButton1 < 0){
             arm1.setPower(-1);
-            arm2.setPower(-1);
+            arm3.setPower(-1);
+
         }else{
             arm1.setPower(0);
             arm2.setPower(0);
+            arm3.setPower(0);
         }
-
+        if (downButton1 > 0){
+            arm2.setPower(1);
+        }
+        else if (downButton1 < 0){
+            arm2.setPower(-1);
+        }
+        else {
+            arm1.setPower(0);
+            arm2.setPower(0);
+            arm3.setPower(0);
+        }
         // send telemetry message to signify robot running
         // %.2f shows two decimal places
         telemetry.addData("leftY",  "%.2f", leftY);
