dd85a5e001491b29cc5d2c4e70cbdf560f1c6edb
Stephie Seaton <stephanie.seaton@sisepuedefoundation.org>
2025-02-10 19:15:14 -0700

    ez claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2024.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Pushing the left  stick forward      makes the robot go forward
@@ -23,7 +24,8 @@ public class TeleOp2024 extends LinearOpMode {
 
     DcMotor arm1;
     DcMotor arm2;
-    DcMotor arm3;
+    Servo arm3;
+    DcMotor arm4;
     MechanumDrive e;
 
 
@@ -39,10 +41,12 @@ public class TeleOp2024 extends LinearOpMode {
         // initialize motors
         arm1 = hardwareMap.get(DcMotor.class, "arm1");
         arm2 = hardwareMap.get(DcMotor.class, "arm2");
-        arm3 = hardwareMap.get(DcMotor.class, "arm3");
+        arm3 = hardwareMap.get(Servo.class, "arm3");
+        arm4 = hardwareMap.get(DcMotor.class, "arm4");
+
         arm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        arm3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 //
 //
@@ -72,35 +76,46 @@ public class TeleOp2024 extends LinearOpMode {
         double leftX = -gamepad1.left_stick_x * 1.1;
         double rightX = gamepad1.right_stick_x;
 
+
+
         e.MoveRobot(leftY,leftX,rightX);
         double upButton1 = gamepad2.left_stick_y;
         double downButton1 = gamepad2.right_stick_y;
-
+        boolean upDpad = gamepad2.dpad_up;
+        boolean downDpad = gamepad2.dpad_down;
+        boolean clawOpen = gamepad2.a;
+        boolean clawClose = gamepad2.b;
         if (upButton1 > 0) {
             arm1.setPower(1);
-            arm3.setPower(1);
+
         }
         //down button is a on the game pad and up button is x on the game pad. when you press a both arms go down. when you press x both arms go up faster than they go down.
         //the if statements check if the two buttons on the gamepad are pressed and do the actions assigned to the button.
         else if (upButton1 < 0){
             arm1.setPower(-1);
-            arm3.setPower(-1);
+
 
         }else{
             arm1.setPower(0);
             arm2.setPower(0);
-            arm3.setPower(0);
+
         }
-        if (downButton1 > 0){
+        if (upDpad){
             arm2.setPower(1);
         }
-        else if (downButton1 < 0){
+        else if (downDpad){
             arm2.setPower(-1);
         }
         else {
             arm1.setPower(0);
             arm2.setPower(0);
-            arm3.setPower(0);
+
+        }
+        if (clawOpen){
+            arm3.setPosition(180);
+        }
+        if (clawClose){
+            arm3.setPosition(-180);
         }
         // send telemetry message to signify robot running
         // %.2f shows two decimal places
